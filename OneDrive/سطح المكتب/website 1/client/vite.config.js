72d0802d43c98fc69f283ca80f3366fd0a80674d
asad200-stack <asad200-stack@users.noreply.github.com>
2026-01-05 04:46:33 +0200

    Add more debugging and simplify vite.config.js

--- OneDrive/سطح المكتب/website 1/client/vite.config.js
+++ OneDrive/سطح المكتب/website 1/client/vite.config.js
@@ -1,9 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 
 export default defineConfig({
-  root: process.cwd(),
   plugins: [react()],
   build: {
     outDir: 'dist',
